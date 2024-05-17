607744794743d667ee27230df53fb26fcd4d0b06
이준희 <@DESKTOP-HR6SU65>
2024-05-17 20:06:22 +0900

    0517

--- project/src/main/webapp/scripts.js
+++ project/src/main/webapp/scripts.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     var loginButton = document.getElementById("loginButton");
     var modalBackground = document.getElementById("modalBackground");
-    var loginModal = document.getElementById("loginModal");
+    var registerModal = document.getElementById("registerModal");
     var closeModal = document.getElementById("closeModal");
 
     loginButton.addEventListener("click", function () {
         modalBackground.style.display = "block";
-        loginModal.style.display = "block";
+        registerModal.style.display = "block";
     });
 
     closeModal.addEventListener("click", function () {
         modalBackground.style.display = "none";
-        loginModal.style.display = "none";
+        registerModal.style.display = "none";
     });
 
     // Close modal when clicking outside of it
     modalBackground.addEventListener("click", function () {
         modalBackground.style.display = "none";
-        loginModal.style.display = "none";
+        registerModal.style.display = "none";
     });
 });
